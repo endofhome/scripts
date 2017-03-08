f1414f6cecf8307c189b89a482bf27528a4a55df
Tom Barnes <ruralmyth@gmail.com>
2017-03-08 22:46:47 +0000

    progress dots are red for MPs who employ family members, white for others

--- which_mps_employ_their_family_members/mps_employ.sh
+++ which_mps_employ_their_family_members/mps_employ.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# colours
+RED=`tput setaf 1`
+RESET=`tput sgr0`
+
 for MP_NAME in $(cat list_of_mps.txt)
 do 
   EMPLOYED=$(curl -s https://www.publications.parliament.uk/pa/cm/cmregmem/170220/$MP_NAME.htm | grep 'I employ' | grep -o '>.*<' | tr -d '<>' | sed 's/^span class="highlight"//' | sed 's/\/span//')
   echo "$MP_NAME,\"$EMPLOYED\"" >> mps_employ.csv
-  printf "."
+  if [[ -z "$EMPLOYED"  ]]; then
+    printf "."
+  else
+    printf "${RED}.${RESET}"
+  fi
 done
 
 printf "\n"
