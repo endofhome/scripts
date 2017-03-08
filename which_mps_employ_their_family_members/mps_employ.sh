1ae07c548abacad198ef424c0cc5837359b0dad5
Tom Barnes <ruralmyth@gmail.com>
2017-03-08 23:43:38 +0000

    nicer progress message with x/y MPs checked and x MPs identified

--- which_mps_employ_their_family_members/mps_employ.sh
+++ which_mps_employ_their_family_members/mps_employ.sh
@@ -4,15 +4,23 @@
 RED=`tput setaf 1`
 RESET=`tput sgr0`
 
+NUMBER_OF_MPS=$(wc -l < list_of_mps.txt | tr -d ' ')
+COUNTER=1
+MPS_FOUND=0
+OUTPUT_FILE="mps_employ.csv"
+
+echo "Scanning MP list..."
+
 for MP_NAME in $(cat list_of_mps.txt)
 do 
   EMPLOYED=$(curl -s https://www.publications.parliament.uk/pa/cm/cmregmem/170220/$MP_NAME.htm | grep 'I employ' | grep -o '>.*<' | tr -d '<>' | sed 's/^span class="highlight"//' | sed 's/\/span//')
-  echo "$MP_NAME,\"$EMPLOYED\"" >> mps_employ.csv
-  if [[ -z "$EMPLOYED"  ]]; then
-    printf "."
-  else
-    printf "${RED}.${RESET}"
+  if [[ -n "$EMPLOYED"  ]]; then
+    let "MPS_FOUND++"
   fi
+  echo "$MP_NAME,\"$EMPLOYED\"" >> $OUTPUT_FILE
+  printf "Checked $COUNTER/$NUMBER_OF_MPS MPs. Found ${RED}$MPS_FOUND${RESET} MPs who employ their family members\r"
+  let "COUNTER++"
 done
 
 printf "\n"
+echo "CSV report saved at $OUTPUT_FILE"
