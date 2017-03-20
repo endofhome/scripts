c7c5e180fd04a28aad46883d4d6df1111003975f
Tom Barnes <ruralmyth@gmail.com>
2017-03-20 23:36:50 +0000

    clean up files on each run and properly use ERROR_FILE variable

--- which_mps_employ_their_family_members/mps_employ.sh
+++ which_mps_employ_their_family_members/mps_employ.sh
@@ -8,10 +8,12 @@ TOTAL_MPS=$(wc -l < list_of_mps.txt | tr -d ' ') && let "TOTAL_MPS++"
 COUNTER=1
 MPS_FOUND=0
 OUTPUT_FILE="mps_employ.csv"
-ERROR_FILE="errors.csv"
+ERROR_FILE="errors_mps_employ.csv"
 ERRORS=false
 
 function main {
+  cleanUp
+
   for MP_NAME in $(cat list_of_mps.txt | stripDOSLineEndings )
   do
     MP_URL="https://www.publications.parliament.uk/pa/cm/cmregmem/170306/$MP_NAME.htm"
@@ -33,13 +35,23 @@ function main {
   reportAnyErrors
 }
 
+function cleanUp {
+  if [[ -s $OUTPUT_FILE ]]; then
+    rm $OUTPUT_FILE
+  fi
+
+  if [[ -s $ERROR_FILE ]]; then
+    rm $ERROR_FILE
+  fi
+}
+
 function stripDOSLineEndings {
   sed 's/^M$//'
 }
 
 function checkForAndLogIfError {
   if [[ -n $(echo "$MP_HTML_PAGE" | grep "<meta property=\"og:title\" content=\"Page cannot be found\"/>") ]]; then
-    echo "$MP_NAME,$MP_URL" >> errors.csv
+    echo "$MP_NAME,$MP_URL" >> $ERROR_FILE
     ERRORS=true
   fi
 }
