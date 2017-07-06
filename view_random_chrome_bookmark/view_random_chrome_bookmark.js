eaa4074e9e08328018a4c8348e5de22a737a4420
Tom Barnes <tom.barnes@springer.com>
2017-07-06 13:33:57 +0100

    view random chrome bookmark properly with recursion

--- view_random_chrome_bookmark/view_random_chrome_bookmark.js
+++ view_random_chrome_bookmark/view_random_chrome_bookmark.js
@@ -1,49 +1,30 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const username = require("os").userInfo().username;
 const opn = require('opn');
 const links = [];
 
-fs.readFile("/Users/tombarnes/Library/Application Support/Google/Chrome/Default/Bookmarks", 'utf-8', function read(err, data) {
+const addLinksAndTraverseTree = function(child) {
+    links.push(child["url"]);
+    if (child.type === "folder") {
+        child["children"].forEach(child => {
+            addLinksAndTraverseTree(child)
+        });
+    }
+};
+
+fs.readFile(`/Users/${username}/Library/Application Support/Google/Chrome/Default/Bookmarks`, 'utf-8', function read(err, data) {
     if (err) {
         throw err;
     }
     const content = JSON.parse(data);
 
-    content["roots"]["bookmark_bar"]["children"].forEach(function (child) {
-        if (child.type === "folder") {
-            child["children"].forEach(function (firstChild) {
-                if (firstChild.type === "folder") {
-                    firstChild["children"].forEach(function (secondChild) {
-                        if (secondChild.type === "folder") {
-                            secondChild["children"].forEach(function (thirdChild) {
-                                if (thirdChild.type === "folder") {
-                                    thirdChild["children"].forEach(function (fourthChild) {
-                                        if (fourthChild.type === "folder") {
-                                            fourthChild["children"].forEach(function(fifthChild) {
-                                                links.push(fifthChild["url"]);
-                                            })
-                                        }
-                                        links.push(fourthChild["url"]);
-                                    });
-                                }
-                                links.push(thirdChild["url"]);
-                            });
-                        }
-                        links.push(secondChild["url"]);
-                    });
-                } else {
-                    links.push(firstChild["url"]);
-                }
-            });
-        } else {
-            links.push(child["url"]);
-        }
+    content["roots"]["bookmark_bar"]["children"].forEach(child => {
+        addLinksAndTraverseTree(child)
     });
+
     const index = Math.random() * links.length;
     opn(links[Math.floor(index)]);
     process.exit(0);
 });
-
-
- 
