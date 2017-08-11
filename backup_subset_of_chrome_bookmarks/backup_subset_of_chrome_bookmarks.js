b1b4b8ec1f4aa651fb7f924abe2f8a2a76c3d3f6
Tom Barnes <ruralmyth@gmail.com>
2017-08-11 23:06:24 +0100

    can backup subset of chrome bookmarks

--- backup_subset_of_chrome_bookmarks/backup_subset_of_chrome_bookmarks.js
+++ backup_subset_of_chrome_bookmarks/backup_subset_of_chrome_bookmarks.js
@@ -3,25 +3,42 @@
 const fs = require('fs');
 const username = require("os").userInfo().username;
 const links = [];
+const outputPath = '/out/bookmarks-backup.json';
+const args = process.argv.slice(2);
 
-const addLinksAndTraverseTree = function(child) {
-    links.push(child["url"]);
-    if (child.type === "folder") {
-        child["children"].forEach(child => {
-            addLinksAndTraverseTree(child)
-        });
+const validated = function(args) {
+    if (args.length > 1) {
+        console.log("Only one argument, the name of a single directory or bookmark, can be provided.");
+        process.exit(1);
     }
+    return args[0]
 };
 
-fs.readFile(`/Users/${username}/Library/Application Support/Google/Chrome/Default/Bookmarks`, 'utf-8', function read(err, data) {
-    if (err) {
-        throw err;
+function writeFileOrPrintError() {
+    if (links.length > 0) {
+        console.log("Writing backup file...");
+        fs.writeFileSync('./out/bookmarks-backup.json', JSON.stringify(links), {}, (err) => {
+            if (err) throw err;
+        });
+        console.log(`Bookmarks backed up to ${outputPath}`);
+    } else {
+        console.log(`No bookmark or directory with the name \"${target}\" found.`)
+        process.exit(1);
     }
+}
+
+const target = validated(args);
+
+fs.readFile(`/Users/${username}/Library/Application Support/Google/Chrome/Default/Bookmarks`, 'utf-8', (err, data) => {
+    if (err) throw err;
     const content = JSON.parse(data);
 
     content["roots"]["bookmark_bar"]["children"].forEach(child => {
-        addLinksAndTraverseTree(child)
+        if (child.name === target) {
+            links.push(child)
+        }
     });
 
+    writeFileOrPrintError();
     process.exit(0);
 });
